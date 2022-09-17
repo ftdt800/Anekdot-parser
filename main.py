c81c6814e577e719e6c6efa15c3381737454762a
ftdt800 <79777228+ftdt800@users.noreply.github.com>
2022-09-17 14:59:12 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿import requests
+from bs4 import BeautifulSoup
+import pyautogui
+import pyperclip
+import keyboard
+
+def get_text(url):
+    rs = requests.get(url)
+    root = BeautifulSoup(rs.content, 'html.parser')
+    article = root.select_one('td')
+
+    return article.text
+
+
+url = 'https://randstuff.ru/joke/'
+while True:
+    text = get_text(url)
+# print(text)
+    print(text[:100])  # Первые 100 символов из строки
+    pyautogui.click(392, 1115)
+    pyperclip.copy(text)
+    keyboard.press_and_release('ctrl + v')
+    pyautogui.press('enter')
