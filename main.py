e3b1227827b668ac7a92aec9ea28dfbcdae60770
NotForDiscord <79777228+ftdt800@users.noreply.github.com>
2023-03-11 12:35:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ import keyboard
 def get_text(url):
     rs = requests.get(url)
     root = BeautifulSoup(rs.content, 'html.parser')
-    article = root.select_one('td')
-
-    return article.text
-
+    article = root.select_one('td')
+    return article.text
 x = 1
 url = 'https://randstuff.ru/joke/'
 while x < 20:
     x += 1
     text = get_text(url)
 # print(text)
-    print(text[:100])  # Первые 100 символов из строки
+    print(text[:100])
     pyautogui.click(392, 1115)
     pyperclip.copy(text)
     keyboard.press_and_release('ctrl + v')
